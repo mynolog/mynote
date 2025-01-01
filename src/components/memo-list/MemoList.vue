13fbeac50236a3384fe396f0886e3f4d962bc300
mynolog <minholee.log@gmail.com>
2025-01-01 21:05:46 +0900

    ✨ feat: MemoList, MemoDetail 컴포넌트 생성 및 RootLayout 추가

---
+++ src/components/memo-list/MemoList.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AppLogo from '../common/app-logo/AppLogo.vue'
+</script>
+
+<template>
+  <div class="w-1/5">
+    <app-logo variant="long" />
+  </div>
+</template>
---
+++ src/layout/RootLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MemoList from '../components/memo-list/MemoList.vue'
+import MemoDetail from '../components/memo-detail/MemoDetail.vue'
+</script>
+
+<template>
+  <div class="flex">
+    <memo-list />
+    <memo-detail />
+  </div>
+</template>
